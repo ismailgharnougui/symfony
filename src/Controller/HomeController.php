478b757b17e556eedc7ba7fd02b3b054d77edd0b
ismae <gharnougui.ismail@gmail.com>
2023-10-02 00:00:05 +0100

    atelier2.2

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/affiche', name: 'app_affiche')]
+    public function Affiche()
+    {
+        return new Response("<h1>Bonjour mes étudiants</h1>");
+    }
+}
